c50b8390c23f85eca887bd7d67537b7669a5c82d
Ondřej Šabrňák <ondrej@sabrnak.com>
2024-12-07 16:02:24 +0100

    feat(createAbl): adding createAbl functionality

--- server/abl/book/createAbl.js
+++ server/abl/book/createAbl.js
@@ -19,7 +19,43 @@ const schema = {
 };
 
 async function createAbl(req, res) {
-  // TODO: Implement the createAbl function
+  try {
+    let book = req.body;
+
+    // Validate the input
+    const valid = ajv.validate(schema, book);
+    if (!valid) {
+      res.status(400).json({
+        code: "dtoInIsNotValid",
+        message: "The input data is not valid",
+        validationErrors: ajv.errors,
+      });
+      return;
+    }
+
+    // Check if a book with the same ISBN already exists
+    if (bookDao.getByIsbn(book.isbn)) {
+      res.status(400).json({
+        code: "isbnAlreadyExists",
+        message: "A book with the same ISBN already exists",
+      });
+      return;
+    }
+
+    // Store the book to persistent storage
+    try {
+      book = bookDao.create(book);
+    } catch (e) {
+      res.status(500).json(...e);
+      return;
+    }
+
+    // Return the created book
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: e.message });
+  }
 }
 
 module.exports = createAbl;
