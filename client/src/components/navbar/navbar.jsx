4bd30dbe14969892dd031632d01970adc381c866
Ondřej Šabrňák <ondrej@sabrnak.com>
2024-12-11 13:03:30 +0100

    feat: adding localization support

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,20 +1,29 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
-import { APP_NAME } from "../../constants/app";
+import { useTranslation } from 'react-i18next';
 
 const NavbarComponent = () => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">{APP_NAME}</Navbar.Brand>
+        <Navbar.Brand href="#home">{t('app_name')}</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Hlavní panel</Nav.Link>
-            <Nav.Link href="#books">Knihy</Nav.Link>
-            <Nav.Link href="#about">O aplikaci</Nav.Link>
+            <Nav.Link href="#home">{t('nav.home')}</Nav.Link>
+            <Nav.Link href="#books">{t('nav.books')}</Nav.Link>
+            <Nav.Link href="#about">{t('nav.about')}</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link onClick={() => changeLanguage('en')}>EN</Nav.Link>
+            <Nav.Link onClick={() => changeLanguage('cs')}>CS</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- client/src/constants/app.js
+++
@@ -1,5 +0,0 @@
-export const APP_NAME = "The Reader's Diary";
-
-// You can add other app-wide constants here
-export const APP_VERSION = "1.0.0";
-export const APP_DESCRIPTION = "A place to keep track of your reading journey.";
