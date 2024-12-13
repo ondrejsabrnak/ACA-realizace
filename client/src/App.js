f1643fafa698f3f9be40781973939078063ddb5f
Ondřej Šabrňák <ondrej@sabrnak.com>
2024-12-13 13:21:02 +0100

    feat: adding testing data

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,37 @@ import Button from "react-bootstrap/Button";
 import NavbarComponent from "./components/navbar/navbar";
 import FooterComponent from "./components/footer/footer";
 
+// Just for testing before joining the backend
+const books = [
+  {
+    title: "Harry Potter a Kámen mudrců",
+    author: "J. K. Rowling",
+    numberOfPages: 288,
+    isbn: "978-80-00-06758-2",
+    id: "539c7ccca3b25df23b2a33e7eb0da733",
+    pagesRead: 75,
+    finished: false,
+  },
+  {
+    title: "Hobit aneb Cesta tam a zase zpátky",
+    author: "J. R. R. Tolkien",
+    numberOfPages: 310,
+    isbn: "978-80-719-5377-2",
+    id: "8c627b526a5945f089f8d342b3c11888",
+    pagesRead: 310,
+    finished: true,
+  },
+  {
+    title: "Pán prstenů: Společenstvo prstenu",
+    author: "J. R. R. Tolkien",
+    numberOfPages: 423,
+    isbn: "978-80-719-5378-9",
+    id: "a7933f720ed04a1dbbf39b8c1323d511",
+    pagesRead: 150,
+    finished: false,
+  },
+];
+
 const App = () => (
   <div className="min-vh-100 d-flex flex-column">
     <header>
