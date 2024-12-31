61ed4dae3af68ef3086990762f11c945e79ab446
Ondřej Šabrňák <ondrej@sabrnak.com>
2024-12-31 09:04:54 +0100

    feat: add readonly prop to StarRating component to control rating changes

--- client/src/components/book/StarRating.js
+++ client/src/components/book/StarRating.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Rating from "react-rating";
 
-const StarRating = ({ rating, onRatingChange }) => {
+const StarRating = ({ rating, onRatingChange, readonly = false }) => {
   return (
     <Rating
       initialRating={rating}
-      onChange={onRatingChange}
+      onChange={readonly ? undefined : onRatingChange}
+      readonly={readonly}
       emptySymbol={<i className="bi bi-star text-warning fs-4"></i>}
       fullSymbol={<i className="bi bi-star-fill text-warning fs-4"></i>}
       fractions={1}
