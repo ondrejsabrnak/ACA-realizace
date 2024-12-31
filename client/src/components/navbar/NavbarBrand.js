039b7ab7ad258164d0678bfb054e0eda41490a57
Ondřej Šabrňák <ondrej@sabrnak.com>
2024-12-31 10:32:50 +0100

    feat: update NavbarBrand to use React Router for navigation

--- client/src/components/navbar/NavbarBrand.js
+++ client/src/components/navbar/NavbarBrand.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 const NavbarBrand = () => {
   const { t } = useTranslation();
 
-  return <Navbar.Brand href="#home">{t("app_name")}</Navbar.Brand>;
+  return (
+    <Navbar.Brand as={Link} to="/">
+      {t("app_name")}
+    </Navbar.Brand>
+  );
 };
 
 export default NavbarBrand;
